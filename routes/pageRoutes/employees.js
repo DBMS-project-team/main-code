093e0d546821674304b68787a043ab7b25e6eed5
Yathu_NK <62744592+YathuNK@users.noreply.github.com>
2021-02-06 16:41:56 +0530

    Add files via upload

--- routes/pageRoutes/employees.js
+++ routes/pageRoutes/employees.js
@@ -3,6 +3,7 @@ const router = express.Router();
 var dateFormat = require('dateformat');
 const db = require('../../db_config');
 const feather = require('feather-icons');
+const fs = require("fs")
 
 router.get('/', (req, res)=>{
     db.query("select e.firstname, e.lastname, e.birthdate, e.martial_status, d.name, p.pay_grade_level_title, e.emp_id, u.emp_id user_id from employees as e inner join departments as d ON e.dept_id=d.dept_id inner join pay_grades as p ON e.pay_grade_level=p.pay_grade_level left join users as u ON e.emp_id=u.emp_id", (error, result)=>{
@@ -21,10 +22,18 @@ router.get('/', (req, res)=>{
 router.get('/edit/:emp_id/*', (req, res)=>{
     try{
         const emp_id = req.params.emp_id;
-        db.query("SELECT * FROM `departments`;SELECT * FROM `job_titles`;SELECT * FROM `employment_statuses`;SELECT * FROM `pay_grades`;SELECT emp_id, concat(firstname,' ',lastname) as fullname FROM `employees` where status=1 and emp_id != ? ;SELECT * FROM `user_levels`; SELECT c.custom_field_id,c.custom_field_name,d.custom_field_value,d.custom_field_id AS old_custom_field_id FROM `custom_fields` c LEFT JOIN (SELECT * FROM employee_additional_detail e WHERE emp_id=?) d ON c.custom_field_id=d.custom_field_id ; SELECT e.emp_id,e.firstname,e.lastname,concat(e.birthdate,'') as bd,e.martial_status,e.dept_id,e.job_id,e.emp_status_id,e.pay_grade_level,e.supervisor,u.username,u.user_level, u.emp_id AS userAcc FROM `employees` e LEFT JOIN `users` u on (e.emp_id=u.emp_id) WHERE e.emp_id=?", [emp_id,emp_id,emp_id],(error, result)=>{
+        db.query("SELECT * FROM `departments`;SELECT * FROM `job_titles`;SELECT * FROM `employment_statuses`;SELECT * FROM `pay_grades`;SELECT emp_id, concat(firstname,' ',lastname) as fullname FROM `employees` where status=1 and emp_id != ? ;SELECT * FROM `user_levels`; SELECT c.custom_field_id,c.custom_field_name,d.custom_field_value,d.custom_field_id AS old_custom_field_id FROM `custom_fields` c LEFT JOIN (SELECT * FROM employee_additional_detail e WHERE emp_id=?) d ON c.custom_field_id=d.custom_field_id ; SELECT e.emp_id,e.firstname,e.lastname,concat(e.birthdate,'') as bd,e.martial_status,e.dept_id,e.job_id,e.emp_status_id,e.pay_grade_level,e.supervisor,u.username,u.user_level, u.emp_id AS userAcc FROM `employees` e LEFT JOIN `users` u on (e.emp_id=u.emp_id) WHERE e.emp_id=?", [emp_id,emp_id,emp_id],
+        (error, result)=>{
             if(error) console.log('mysql error', error);
             else {
-                res.render('employees/newEmployee',{result,newEmp:false});
+                fs.readFile("public\\img\\profile\\"+result[7][0].emp_id+".jpg",{encoding: 'base64'}, function(err, image) {
+                    if (err) imageData="default"; 
+                    else{
+                        imageData="data:image/png;base64,"+image
+                    }
+                    res.render('employees/newEmployee',{result,newEmp:false,imageData}); 
+                  });
+                
             } 
         })
     } catch (error) {
